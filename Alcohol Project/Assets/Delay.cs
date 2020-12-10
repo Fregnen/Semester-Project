8c0cd8af9e006f069877d5fb8183915fcf0f8ec5
maceq687 <maciek@softman.pl>
2020-12-10 17:12:21 +0100

    Added headtracker, AudioSource (incl delay) and folders for video and audio files

---
+++ Alcohol Project/Assets/Delay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delay : MonoBehaviour
+
+{
+    public AudioSource AudioSource;
+    public float delay = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        AudioSource.PlayDelayed(delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
