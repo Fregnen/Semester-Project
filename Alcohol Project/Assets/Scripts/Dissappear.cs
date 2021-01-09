1c9d1f707c6220f4f10a255145e55e143fcc21ca
maceq687 <maciek@softman.pl>
2021-01-09 17:32:55 +0100

    Dissapear script + scripts folder

---
+++ Alcohol Project/Assets/Scripts/Dissappear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissappear : MonoBehaviour
+{
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        //Start the coroutine
+        StartCoroutine(WaitCo());
+    }
+
+    IEnumerator WaitCo()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        gameObject.SetActive(false);
+    }
+}
